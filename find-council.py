fdab363544cc2e2cc567f1d2936f55e78b1182f9
poo5zan <pujanmaharjan@gmail.com>
2024-02-13 23:28:23 +1030

    updated find council method with timeout feature

--- find-council.py
+++ find-council.py
@@ -6,9 +6,21 @@ from selenium.webdriver.common.by import By
 import selenium.webdriver as webdriver
 import time
 from requests.utils import quote
+from datetime import datetime
+import time
 
-def find_council_by_address(address):
+def extract_value_replacing_prefix(text_array, prefix):
+    values = [t for t in text_array if t.startswith(prefix)]
+    if len(values) > 0:
+        return values[0].replace(prefix, '').strip()
+        
+    return ''
 
+def find_council_by_address(address, timeout_in_seconds = 600):
+    if address is None or address == '':
+        raise Exception('address is required')
+    start_time = datetime.now()
+    default_time_to_wait_in_seconds = 5
     with webdriver.Chrome() as driver:
         address_encoded = quote(address)
         url = f'https://lga-sa.maps.arcgis.com/apps/instant/lookup/index.html?appid=db6cce7b773746b4a1d4ce544435f9da&find={address_encoded}'
@@ -16,30 +28,34 @@ def find_council_by_address(address):
         to_exclude = ['Results:1', '', 'Loading...']
         driver.get(url)
         text = ''
-        while True:        
-            for row in WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@id="resultsPanel"]'))):
-                time.sleep(5)
-                text = row.text
-                print('text ', text)
-
+        while True:   
+            rows = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@id="resultsPanel"]')))
+            if len(rows) > 0:
+                text = rows[0].text
+            # wait some seconds before next try
+            time_to_sleep_in_seconds = default_time_to_wait_in_seconds if timeout_in_seconds > default_time_to_wait_in_seconds else timeout_in_seconds
+            time.sleep(time_to_sleep_in_seconds)
             if text not in to_exclude:
                 break
-                
+
+            # if we couldnot find data in timeout, then simply ignore it, else there is possibility of infinite loop
+            difference_time = datetime.now() - start_time
+            if difference_time.total_seconds() > timeout_in_seconds:
+                print(f'Wait Timeout of {timeout_in_seconds} seconds exceeded')
+                break
+
+    if text == '':
+        print('Couldnot retrieve data. So, return None')
+        return None
+    
     print('text value ', text)
     text_array = text.splitlines()
-    print('text array ', text_array)
 
-    def extract_value(text_array, prefix):
-        values = [t for t in text_array if t.startswith(prefix)]
-        if len(values) > 0:
-            return values[0].replace(prefix, '').strip()
-        
-        return ''
-
-    council_name = extract_value(text_array, "Council Name")
-    electoral_ward = extract_value(text_array, "Electoral Ward")
+    council_name = extract_value_replacing_prefix(text_array, "Council Name")
+    electoral_ward = extract_value_replacing_prefix(text_array, "Electoral Ward")
 
     return {'address': address, 'council_name': council_name, 'electoral_ward': electoral_ward}
 
-council_details = find_council_by_address("130 L'Estrange Street, Glenunga")
-print('council details ', council_details)
+print('council details ', find_council_by_address("130 L'Estrange Street, Glenunga"))
+
+print(find_council_by_address("130 L'Estrange Street, Glenunga", 2))
