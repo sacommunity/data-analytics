53743ee41662deee7a5b6cd24016d589589cf3dc
poo5zan <pujanmaharjan@gmail.com>
2024-02-21 01:18:03 +1030

    auto format python code using autopep8

--- web_scraping.py
+++ web_scraping.py
@@ -11,12 +11,14 @@ from selenium.common.exceptions import TimeoutException as SeleniumTimeoutExcept
 import backoff
 import logging
 
+
 def extract_value_replacing_prefix(text_array, prefix):
     values = [t for t in text_array if t.startswith(prefix)]
     if len(values) > 0:
         return values[0].replace(prefix, '').strip()
-        
+
     return ''
+
 
 def get_chrome_options(is_headless):
     options = webdriver.ChromeOptions()
@@ -25,24 +27,29 @@ def get_chrome_options(is_headless):
 
     return options
 
-def on_backoff_handler(details):
-    logging.debug("Backing off {wait:0.1f} seconds after {tries} tries calling function {target} with args {args} and kwargs {kwargs}".format(**details))
 
-@backoff.on_exception(backoff.expo, SeleniumTimeoutException, max_tries = 3, on_backoff = on_backoff_handler)
-def get_data_from_url(url: str, is_headless: 
-                      bool, to_exclude: list, 
-                      timeout_in_seconds: int, 
+def on_backoff_handler(details):
+    logging.debug(
+        "Backing off {wait:0.1f} seconds after {tries} tries calling function {target} with args {args} and kwargs {kwargs}".format(**details))
+
+
+@backoff.on_exception(backoff.expo, SeleniumTimeoutException, max_tries=3, on_backoff=on_backoff_handler)
+def get_data_from_url(url: str,
+                      is_headless: bool,
+                      to_exclude: list,
+                      timeout_in_seconds: int,
                       xpath: str):
     start_time = datetime.now()
     default_time_to_wait_in_seconds = 5
-    
+
     options = get_chrome_options(is_headless)
     with webdriver.Chrome(options=options) as driver:
         logging.info('Fetching data from url ', url)
         driver.get(url)
         text = ''
-        while True:   
-            rows = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, xpath)))
+        while True:
+            rows = WebDriverWait(driver, 20).until(
+                EC.visibility_of_all_elements_located((By.XPATH, xpath)))
             if len(rows) > 0:
                 text = rows[0].text
             # wait some seconds before next try
@@ -64,9 +71,9 @@ def get_data_from_url(url: str, is_headless:
     return text
 
 # Manual retry logic. I ended up using backoff library instead. Kept here just for reference
-# def get_data_from_url_with_retry(url: str, is_headless: 
-#                       bool, to_exclude: list, 
-#                       timeout_in_seconds: int, 
+# def get_data_from_url_with_retry(url: str, is_headless:
+#                       bool, to_exclude: list,
+#                       timeout_in_seconds: int,
 #                       xpath: str):
 #     MAX_RETRY_COUNT = 3
 
@@ -82,7 +89,7 @@ def get_data_from_url(url: str, is_headless:
 #             print('General Exception ', str(ex))
 #             print('Exception type ', type(ex))
 #             return None
-    
+
 
 '''
     Finds council by address
@@ -90,21 +97,25 @@ def get_data_from_url(url: str, is_headless:
     timeout_in_seconds: timeout in seconds until which the program will wait before returning None
     is_headless: if False, a chrome browser will popup, else the operation will be done in background
 '''
-def find_council_by_address(address, timeout_in_seconds = 600, is_headless = True):
+
+
+def find_council_by_address(address, timeout_in_seconds=600, is_headless=True):
     if address is None or address == '':
         raise Exception('address is required')
-    
+
     address_encoded = quote(address)
     url = f'https://lga-sa.maps.arcgis.com/apps/instant/lookup/index.html?appid=db6cce7b773746b4a1d4ce544435f9da&find={address_encoded}'
     logging.info('Fetching council for ', address)
     to_exclude = ['Results:1', '', 'Loading...']
-    text = get_data_from_url(url, is_headless, to_exclude, timeout_in_seconds, '//*[@id="resultsPanel"]')
+    text = get_data_from_url(url, is_headless, to_exclude,
+                             timeout_in_seconds, '//*[@id="resultsPanel"]')
     if text == '':
         return ''
     text_array = text.splitlines()
 
     council_name = extract_value_replacing_prefix(text_array, "Council Name")
-    electoral_ward = extract_value_replacing_prefix(text_array, "Electoral Ward")
+    electoral_ward = extract_value_replacing_prefix(
+        text_array, "Electoral Ward")
 
     return {'address': address, 'council_name': council_name, 'electoral_ward': electoral_ward}
 
@@ -115,31 +126,35 @@ def find_council_by_address(address, timeout_in_seconds = 600, is_headless = Tru
 # with less timeout, to check test timeout feature. Without timeout, there is possibility of infinite loop
 # print(find_council_by_address("130 L'Estrange Street, Glenunga", 2))
 
-def find_address_from_sacommunity_website(url: str, is_headless = True, timeout_in_seconds = 600):
+
+def find_address_from_sacommunity_website(url: str, is_headless=True, timeout_in_seconds=600):
     if url is None or url == '':
         raise Exception('url is required')
-    
+
     return get_data_from_url(url, is_headless, [], timeout_in_seconds, '//*[@id="content-area"]/div/div[1]/div[2]/div[2]/div/div[1]/div[1]/div[2]')
 
 # test function, useful while debugging
 # url = 'https://sacommunity.org/org/208832-Burnside_Youth_Club'
 # find_address_in_sacommunity(url, False)
 
-def find_addresses_from_sacommunity_website(urls: list, is_headless = True, timeout_in_seconds = 600):
+
+def find_addresses_from_sacommunity_website(urls: list, is_headless=True, timeout_in_seconds=600):
     # maximum number of concurrent requests at a time
     MAXIMUM_CONCURRENT_REQUESTS = 3
     semaphore = Semaphore(MAXIMUM_CONCURRENT_REQUESTS)
 
     threads = []
+
     def find_addr(url, all_address):
         addr = ''
         with semaphore:
             try:
-                addr = find_address_from_sacommunity_website(url, is_headless, timeout_in_seconds)
+                addr = find_address_from_sacommunity_website(
+                    url, is_headless, timeout_in_seconds)
             except Exception as ex:
                 # simply log the exception, don't raise it further
                 logging.error(ex, exc_info=True)
-            
+
         all_address[url] = addr
 
     all_address = {}
