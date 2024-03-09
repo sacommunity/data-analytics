4e8773bf3bcb76798d26b684cbe77f33bd8b9778
poo5zan <pujanmaharjan@gmail.com>
2024-03-09 22:04:02 +1030

    nextjs ui project

---
+++ ui/da-app/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Data Analytics",
+  description: "Web UI for Data Analytics Team in SA Community",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
